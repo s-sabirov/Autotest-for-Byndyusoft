a0d06719d096d18951b1074886391fac75d1f6c2
unknown <serg.sabirowww@gmail.com>
2023-11-21 12:12:29 +0300

    update script

--- cypress/integration/script.js
+++ cypress/integration/script.js
@@ -1,4 +1,4 @@
-it('should find web site', ()=>{   
+it('should find web-site', ()=>{   
     cy.visit('https://www.google.com/'); //переход на страницу Google
     cy.get('#APjFqb').type('Byndyusoft').type('{enter}'); //ввод текста и клик на кнопку Enter
     cy.get('#rso > div:nth-child(1) > div > div > div > div > div > div > div > div.yuRUbf > div > span > a')
@@ -7,7 +7,7 @@ it('should find web site', ()=>{
         cy.request(link.prop('href')).its('status').should('eq',  200); //проверка кода ответа страницы
     });
 });
-it('should open web site', ()=>{   
+it('should open web-site', ()=>{   
     cy.visit('https://byndyusoft.com/'); //переход на страницу Byndyusoft
     cy.get('.knowMore__container > .btn').click(); //клик по кнопке заказать презентацию    
     cy.get('.popup-callback__contacts-tg')
